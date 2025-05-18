fa6ca4d1196e476dd81e8626cc0fdeff97ff1a41
andresinho20049 <andre.andresinho2009@hotmail.com>
2025-05-18 12:42:45 -0300

    feat: Add LoadingContext for managing loading state in the application

---
+++ src/context/LoadingContext.tsx
@@ -0,0 +1,32 @@
+import { createContext, useContext, useState } from "react";
+
+type LoadingContextType = {
+  loading: boolean;
+  setLoading: (loading: boolean) => void;
+};
+
+const LoadingContext = createContext<LoadingContextType>(
+  {} as LoadingContextType
+);
+
+export const useLoading = () => {
+  const context = useContext(LoadingContext);
+  if (!context) {
+    throw new Error("useLoading must be used within a LoadingProvider");
+  }
+  return context;
+};
+
+export const LoadingProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <LoadingContext.Provider value={{ loading, setLoading }}>
+      {children}
+    </LoadingContext.Provider>
+  );
+};
